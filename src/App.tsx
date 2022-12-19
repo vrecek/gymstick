736e68f75f49af48626845830ed37e96b0391447
vrecek <oktawiusz2002@gmail.com>
2022-12-19 17:00:19 +0100

    Hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './css/index.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {HashRouter, Route, Routes} from 'react-router-dom'
 import MAIN_PAGE from './components/Main Page/MAIN_PAGE';
 import CONTACT_PAGE from './components/Contact Page/CONTACT_PAGE';
 import SHOP_PAGE from './components/Shop Page/SHOP_PAGE';
@@ -15,7 +15,7 @@ function App() {
     return (
         <div className="App">
             
-            <Router>
+            <HashRouter>
 
                 <Routes>
 
@@ -34,7 +34,7 @@ function App() {
 
                 <LayoutArrow />
 
-            </Router>
+            </HashRouter>
 
         </div>
     )
